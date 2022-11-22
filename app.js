fed653b1946d7825bb0c5f3e9c7eb1169e4cf8a5
jorabeksobirov <jorabeks298@gmail.com>
2022-11-22 19:04:19 +0500

    string

---
+++ app.js
@@ -0,0 +1,24 @@
+//?String
+
+
+let name = "Mening ismim Jo'rabek"
+let surname = "Familiyam Sobirov" 
+var young = "Yoshim  15 da"
+let accommodation = "Turar joyim Do'nqorg'on"
+var yearofbirth = "Tugu'gan yilim 2007"
+
+
+console.log(name);
+console.log(surname);
+console.log(young);
+console.log(yearofbirth);
+
+
+//?String concatenatoin
+
+var location = "O'qish joyi"
+var Placeofstudy ="Piskent tumani "
+var school = "19-Maktab"
+let full = Placeofstudy + ' ' + school
+
+console.log(location, full);
